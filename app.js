975bb698d81abd2973ad4f7dd42d0981679dc190
Chamon Roy <chamonroy@gmail.com>
2020-10-25 18:41:17 +0600

    TODO machanism complete, Creat, Delete, Sort, Mark Complete features working fine.

---
+++ app.js
@@ -0,0 +1,93 @@
+// Selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
+
+// Event listeners
+todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
+filterOption.addEventListener("click", filterTodo);
+
+//Functions
+function addTodo(event) {
+  // Preventing form from submitting
+  event.preventDefault();
+
+  // Creating TODO div
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+
+  // Creating TODO li elements & appending it in the todo div
+  const newTodo = document.createElement("li");
+
+  // Testing
+  newTodo.innerText = todoInput.value;
+
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo);
+
+  // CHeck button in completion
+  const completeButton = document.createElement("button");
+  completeButton.innerHTML = '<i class="fas fa-check"></i>';
+  completeButton.classList.add("complete-btn");
+  todoDiv.appendChild(completeButton);
+
+  // CHeck button in completion
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+  trashButton.classList.add("trash-btn");
+  todoDiv.appendChild(trashButton);
+
+  // Append to list
+  todoList.appendChild(todoDiv);
+
+  // Clear TODO input value
+  todoInput.value = "";
+}
+
+// Deleting & Check mark on completion Function
+function deleteCheck(e) {
+  const item = e.target;
+
+  //Delete todo
+  if (item.classList[0] === "trash-btn") {
+    const todo = item.parentElement;
+    // Animation to fall
+    todo.classList.add("fall");
+    todo.addEventListener("transitionend", () => {
+      todo.remove();
+    });
+  }
+
+  // Check mark on completing
+  if (item.classList[0] === "complete-btn") {
+    const todo = item.parentElement;
+    todo.classList.toggle("completed");
+  }
+}
+
+function filterTodo(e) {
+  const todos = todoList.childNodes;
+  todos.forEach(function (todo) {
+    switch (e.target.value) {
+      case "all":
+        todo.style.display = "flex";
+        break;
+      case "completed":
+        if (todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+      case "uncompleted":
+        if (!todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+    }
+  });
+}
